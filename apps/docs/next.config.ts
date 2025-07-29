8cdaa74efd000ef2ff146c5909e893dd8c60c5b1
Alex <argeares97@gmail.com>
2025-07-29 14:43:04 +0300

    fix: build

--- apps/docs/next.config.ts
+++ apps/docs/next.config.ts
@@ -6,28 +6,4 @@ const withNextra = nextra({
 });
 
 // Export the final Next.js config with Nextra included
-export default withNextra({
-  webpack(config) {
-    const fileLoaderRule = config.module.rules.find((rule: any) => rule.test?.test?.('.svg'));
-
-    config.module.rules.push(
-      {
-        ...fileLoaderRule,
-        test: /\.svg$/i,
-        issuer: /\.[jt]sx?$/,
-        resourceQuery: /url/, // *.svg?url
-        use: ['@svgr/webpack'],
-      },
-      {
-        test: /\.svg$/i,
-        issuer: /\.[jt]sx?$/,
-        resourceQuery: { not: /url/ }, // exclude if *.svg?url
-        use: ['@svgr/webpack'],
-      },
-    );
-
-    fileLoaderRule.exclude = /\.svg$/i;
-
-    return config;
-  },
-});
+export default withNextra({});
